1bf1afebee0102cc3d7a4f4edaa92ff7f2b672aa
kyplyk <avdosev1@yandex.ru>
2019-07-05 12:22:33 +0400

    small fix for production

--- database/config/database.js
+++ database/config/database.js
@@ -19,7 +19,7 @@ module.exports =
         "logging": true
     },
     "production": {
-        "username": process.env.USERNAME,
+        "username": process.env.LOGININDATABASE,
         "password": process.env.PASSWORD,
         "database": process.env.DATABASE,
         "host": process.env.HOST,
