b0dc72f67ddf48710b11fe0f46180ef9725791a7
kyplyk <avdosev1@yandex.ru>
2019-07-11 20:22:45 +0400

    опять забыл добавить файлики

---
+++ services/markdown.js
@@ -0,0 +1,17 @@
+const md = require('markdown-it')({
+    html: true,
+    linkify: true,
+    typographer: true
+});
+
+// меняем правила игры
+md.renderer.rules.table_open = () => { return '<pre class="pre_table"><div class="scroll_inner_pre"><table>'}
+md.renderer.rules.table_close = () => { return '</table></div></pre>'}
+
+function MarkdownToHtml(str) {
+    return md.render(str);
+}
+
+module.exports = {
+    MarkdownToHtml
+}
